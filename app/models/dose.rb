ca8417309d80d9743a28b5e1f82a503399622d3a
Atila Lima <atilamanager@gmail.com>
2021-02-19 03:13:27 +0900

    All models created

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,7 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
 
-  validates :description, presence: true
-  validates :cocktail, presence: true, length: { minimum: 1 }
-  validates :ingredient, presence: true
+  validates :description, :ingredient, :cocktail, presence: true
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
 end
