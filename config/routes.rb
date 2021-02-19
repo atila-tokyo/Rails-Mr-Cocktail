0d76c33b09528857a0def7f7074cf687d49b09db
Atila Lima <atilamanager@gmail.com>
2021-02-19 18:59:05 +0900

    Created Cocktail controller methods and htmls at view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/create'
+  # get 'cocktails/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :doses
+  resources :cocktails, only: [:show, :new, :create] do
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
+  root to: 'cocktails#index'
 end
