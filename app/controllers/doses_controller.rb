74e4fc832930ea56e1b99650b8fe8f49e9820b74
Atila Lima <atilamanager@gmail.com>
2021-02-19 21:16:47 +0900

    Adding final view still missing style

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -21,6 +21,7 @@ class DosesController < ApplicationController
     @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@dose.cocktail), notice: "Successfuly deleted"
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
+  # get 'doses/new'
+  # get 'doses/create'
   # get 'cocktails/index'
   # get 'cocktails/show'
   # get 'cocktails/create'
   # get 'cocktails/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:show, :new, :create] do
+  resources :cocktails, except: [:update, :edit] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
